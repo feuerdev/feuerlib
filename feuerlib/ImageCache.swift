5e0c3b5f90d62ab7c6d31c93225c3767b4f8e1d7
Jannik Feuerhahn <jannik@feuer.dev>
2020-12-01 15:45:12 +0800

    initial commit

---
+++ feuerlib/ImageCache.swift
@@ -0,0 +1,8 @@
+//
+//  ImageCache.swift
+//  feuerlib
+//
+//  Created by Jannik Feuerhahn on 01.12.20.
+//
+
+import Foundation
---
+++ feuerlib/ServiceError.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceError.swift
+//  feuerlib
+//
+//  Created by Jannik Feuerhahn on 01.12.20.
+//
+
+import Foundation
