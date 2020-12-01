51fe210f9e4f2d06e3cf50bf4a2d78773992d6e4
Jannik Feuerhahn <jannik@feuer.dev>
2020-12-01 15:46:41 +0800

    initial commit

--- feuerlib/ServiceError.swift
+++ feuerlib/ServiceError.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+public enum ServiceError: Error {
+    case badUrl
+    case noNetwork
+    case noData
+    case parserError
+    case couldNotWrite
+    case wrongFiletype
+}
+
+extension ServiceError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .badUrl:
+            return "Bad URL"
+        case .noNetwork:
+            return "No network connection"
+        case .noData:
+            return "No data found"
+        case .parserError:
+            return "Couldn't parse data"
+        case .couldNotWrite:
+            return "Couldn't write to file"
+        case .wrongFiletype:
+            return "Wrong filetype"
+        }
+    }
+}
+
+
