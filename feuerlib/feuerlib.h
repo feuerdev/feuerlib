79a1a83751cfad00afc222ade541e7b08e3ff443
Jannik Feuerhahn <jannik@feuer.dev>
2020-12-01 13:38:01 +0800

    Initial Commit

---
+++ feuerlib/feuerlib.h
@@ -0,0 +1,18 @@
+//
+//  feuerlib.h
+//  feuerlib
+//
+//  Created by Jannik Feuerhahn on 01.12.20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for feuerlib.
+FOUNDATION_EXPORT double feuerlibVersionNumber;
+
+//! Project version string for feuerlib.
+FOUNDATION_EXPORT const unsigned char feuerlibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <feuerlib/PublicHeader.h>
+
+
