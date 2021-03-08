8700cded468f84e3b90d71d54ec8974d2f99926a
Jannik Feuerhahn <jannik@feuer.dev>
2021-03-08 10:47:01 +0800

    added normalization and division functions to Vector3

--- feuerlib/Util/Vector3.swift
+++ feuerlib/Util/Vector3.swift
@@ -19,6 +19,10 @@ public struct Vector3 {
         return (x*x+y*y+z*z).squareRoot()
     }
     
+    public func normalize() -> Vector3 {
+        return self / self.length()
+    }
+    
     static public func + (_ v1:Vector3, _ number:Float) -> Vector3 {
         return .init(v1.x+number, v1.y+number, v1.z+number)
     }
@@ -37,6 +41,10 @@ public struct Vector3 {
     
     static public func / (_ v1:Vector3, _ number:Float) -> Vector3 {
         return .init(v1.x/number, v1.y/number, v1.z/number)
+    }
+    
+    static public func / (_ v1:Vector3, _ v2:Vector3) -> Vector3 {
+        return Vector3(v1.x/v2.x, v1.y/v2.y, v1.z/v2.z)
     }
     
     static public func dot(_ v1:Vector3, _ v2:Vector3) -> Float {
