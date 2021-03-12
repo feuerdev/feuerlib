107fcb6a1ffdcc884eddceb604b5d6a9ad41c9fb
Jannik Feuerhahn <jannik@feuer.dev>
2021-03-12 14:05:27 +0800

    added profiling helpers

--- feuerlib/General.swift
+++ feuerlib/General.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 
-/*
- Returns the CFBundleName key from the main bundle
- */
+
+///Returns the CFBundleName key from the main bundle
 public func getAppName() -> String? {
     return Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String
+}
+
+/// Prints out the time it took for the closure to run
+public func profile(title:String, operation:() -> Void) {
+    let timeElapsed = profile {
+        operation()
+    }
+    print("Time elapsed for \(title): \(timeElapsed) s.")
+}
+
+///Returns the time it took for the closure to run
+public func profile(operation:() -> Void) -> TimeInterval {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    operation()
+    let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+    return timeElapsed
 }
