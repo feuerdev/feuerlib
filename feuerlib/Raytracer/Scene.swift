cd028a9a1c70eba45838c94e97a3914c46258633
Jannik Feuerhahn <jannik@feuer.dev>
2021-04-10 18:00:13 +0800

    updated default scene

--- feuerlib/Raytracer/Scene.swift
+++ feuerlib/Raytracer/Scene.swift
@@ -38,23 +38,24 @@ public struct Scene: Hashable {
     
     ///Manually defined Scene for testing purposes
     public static let testScene:Scene = {
-        let testSpheres = [
-            Sphere(center:Vector3(0.3, 0.5, 0), radius: 0.5, color: .black, specular: 1000, reflectivity: 0.2),
-            Sphere(center:Vector3(0, 3, 2), radius: 2, color: .red, specular: 100000, reflectivity: 0.05),
-            Sphere(center: Vector3(1, 0, 3), radius: 1, color: .blue, specular: 500, reflectivity: 0.08),
-            Sphere(center: Vector3(-2, 0, 3), radius: 1, color: .green, specular: 10, reflectivity: 0.06),
-            Sphere(center: Vector3(0, -5001, 0), radius: 5000, color: .white, specular: 1000, reflectivity: 0.02)
-        ]
+        let testSpheres:[Sphere] = [
+            Sphere(center:Vector3(-1, 0.3, 2), radius: 1.3, color: .red, specular: 5, reflectivity: 0.12),
+            Sphere(center: Vector3(1, 0, 2.5), radius: 1, color: .blue, specular: 1000, reflectivity: 0.38),
+            Sphere(center: Vector3(-0.5, 0, 0), radius: 0.7, color: .green, specular: 500, reflectivity: 0.2),
+            Sphere(center:Vector3(1, -0.5, 0), radius: 0.5, color: .white, specular: 1000, reflectivity: 0.6),
+            Sphere(center: Vector3(0, -5001, 0), radius: 5000, color: .white, specular: 2, reflectivity: 0.02)
+                    ]
         let testLights = [
-            Light(type: .ambient, intensity: 0.3),
-            Light(type: .point, intensity: 0.9, position: .init(2, 3, -1)),
-//            Light(type: .directional, intensity: 0.7, direction: .init(1, 4, 4))
+            Light(type: .ambient, intensity: 0.5),
+            Light(type: .directional, intensity: 0.5, direction: .init(0.5, 1, 0.2))
         ]
         let scene = Scene(
             spheres: testSpheres,
             lights: testLights,
             background: .init(107,159,227),
-            camera: Camera(position: .init(4.5, 2.9, -3), pitch: -0.7, roll: 0, yaw: 0.08))
+            reflections: 3,
+            showLights: false,
+            camera: Camera(position: .init(4.5, 2.9, -3), pitch: -0.8, roll: 0.34, yaw: 0.08))
         return scene
     }()
 }
